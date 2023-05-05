004d7fe1a35011880b142b1fd6cea284bacf9aef
Ethan Alderson <ethanalderson777@gmail.com>
2023-05-05 17:50:58 -0400

    wrote initial critic network

--- ppo.py
+++ ppo.py
@@ -1,5 +1,10 @@
 
 class PPO:
 
+    def __init__(self, env):
+        self.env = env
+        self.obs_dim = env.observation_space.shape[0]
+        self.act_dim = env.action_space.shape[0]
+
     def learn(self):
         pass
